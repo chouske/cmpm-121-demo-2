1d47f96cb531ad10edb06a70a88a864d5c60ac2b
Chase Houske <chouske@ucsc.edu>
2023-10-16 19:14:30 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Test commit";
+const gameName = "Chase's amazing app";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
+const mycanvas = document.createElement("canvas");
+mycanvas.width = 256;
+mycanvas.height = 256;
 header.innerHTML = gameName;
+const ctx = mycanvas.getContext("2d");
+//ctx.fillStyle = "green";
+//ctx.fillRect(0, 0, 256, 256);
 app.append(header);
+app.append(mycanvas);

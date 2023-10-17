0f3e1f8c37331a59db131c08efcb01d1275698de
Chase Houske <chouske@ucsc.edu>
2023-10-16 20:57:38 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,34 @@ document.title = gameName;
 
 const header = document.createElement("h1");
 const mycanvas = document.createElement("canvas");
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+const ctx = mycanvas.getContext("2d");
 mycanvas.width = 256;
 mycanvas.height = 256;
+const cursor = { active: false, x: 0, y: 0 };
+mycanvas.addEventListener("mousedown", (event) => {
+  cursor.active = true;
+  cursor.x = event.offsetX;
+  cursor.y = event.offsetY;
+});
+mycanvas.addEventListener("mousemove", (event) => {
+  if (cursor.active) {
+    ctx?.beginPath();
+    ctx?.moveTo(cursor.x, cursor.y);
+    ctx?.lineTo(event.offsetX, event.offsetY);
+    ctx?.stroke();
+    cursor.x = event.offsetX;
+    cursor.y = event.offsetY;
+  }
+});
+mycanvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+clearButton.addEventListener("click", () => {
+  ctx?.clearRect(0, 0, mycanvas.width, mycanvas.height);
+});
 header.innerHTML = gameName;
 app.append(header);
 app.append(mycanvas);
+app.append(clearButton);

a9c82cec63ebf2a546a3761a112a66f98ddd1bc6
Chase Houske <chouske@ucsc.edu>
2023-10-17 21:19:18 -0700

    Step 5 complete(hopefully i did it right)

--- src/main.ts
+++ src/main.ts
@@ -11,37 +11,65 @@ const mycanvas = document.createElement("canvas");
 const clearButton = document.createElement("button");
 const undoButton = document.createElement("button");
 const redoButton = document.createElement("button");
+class CustomLine {
+  coordinates: { x: number; y: number }[] = [];
+  display(ctx: CanvasRenderingContext2D) {
+    if (this.coordinates.length > 1) {
+      ctx?.beginPath();
+      const { x, y } = this.coordinates[0];
+      ctx?.moveTo(x, y);
+      for (const { x, y } of this.coordinates) {
+        ctx?.lineTo(x, y);
+      }
+      ctx?.stroke();
+    }
+  }
+  add(in1: number, in2: number) {
+    this.coordinates.push({ x: in1, y: in2 });
+  }
+}
 const EMPTY_LIST = 0;
 clearButton.innerHTML = "clear";
 undoButton.innerHTML = "undo";
 redoButton.innerHTML = "redo";
-const ctx = mycanvas.getContext("2d");
+const ctx = mycanvas.getContext("2d")!;
 mycanvas.width = 256;
 mycanvas.height = 256;
 const cursor = { active: false, x: 0, y: 0 };
-const lines: { x: number; y: number }[][] = [];
-const redoStack: { x: number; y: number }[][] = [];
-let currentLine: { x: number; y: number }[] = [];
+//const lines: { x: number; y: number }[][] = [];
+//const redoStack: { x: number; y: number }[][] = [];
+//let currentLine: { x: number; y: number }[] = [];
+const lines: CustomLine[] = [];
+const redoStack: CustomLine[] = [];
+let currentLine: CustomLine = new CustomLine();
 const changedDrawing: Event = new Event("drawing-changed");
+
 mycanvas.addEventListener("mousedown", (event) => {
   cursor.active = true;
   cursor.x = event.offsetX;
   cursor.y = event.offsetY;
-  currentLine = [];
+  //currentLine = [];
+  currentLine = new CustomLine();
   lines.push(currentLine);
   redoStack.splice(0, redoStack.length);
-  currentLine.push({ x: cursor.x, y: cursor.y });
+  //currentLine.push({ x: cursor.x, y: cursor.y });
+  currentLine.add(cursor.x, cursor.y);
 });
 mycanvas.addEventListener("mousemove", (event) => {
   if (cursor.active) {
     cursor.x = event.offsetX;
     cursor.y = event.offsetY;
-    currentLine.push({ x: cursor.x, y: cursor.y });
+    //currentLine.push({ x: cursor.x, y: cursor.y });
+    currentLine.add(cursor.x, cursor.y);
     mycanvas.dispatchEvent(changedDrawing);
   }
 });
 mycanvas.addEventListener("drawing-changed", () => {
   ctx?.clearRect(0, 0, mycanvas.width, mycanvas.height);
+  lines.forEach((element) => {
+    element.display(ctx);
+  });
+  /*ctx?.clearRect(0, 0, mycanvas.width, mycanvas.height);
   lines.forEach((element) => {
     //element is a currentline(array)
     if (element.length > 1) {
@@ -53,8 +81,7 @@ mycanvas.addEventListener("drawing-changed", () => {
       }
       ctx?.stroke();
     }
-  });
-
+  });*/
   //cursor.x = event.offsetX;
   //cursor.y = event.offsetY;
 });
